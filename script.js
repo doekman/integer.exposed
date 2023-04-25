955161294dc82f9dcacdb1e387df09dd6724dfbd
Doeke Zanstra <doekman@icloud.com>
2023-04-25 17:13:36 +0200

    Static version

--- script.js
+++ script.js
@@ -1,133 +1,77 @@
-const app = Vue.createApp({
-  data() {
-    return {
-      value: BigInt(0),
-      bits: 16,
-    };
-  },
-  mounted() {
-    window.addEventListener("hashchange", this.onHashChange);
-    this.onHashChange(); // set the initial values from the hash
-  },
-  computed: {
-    unsignedValue() {
-      return this.value;
-    },
-    signedValue() {
-      return this.value < BigInt(2) ** (BigInt(this.bits) - BigInt(1))
-        ? this.value
-        : this.value - BigInt(2) ** BigInt(this.bits);
-    },
-    hexValue() {
-      return this.value.toString(16);
-    },
-    binaryValue() {
-      return this.value.toString(2);
-    },
-    paddedBinaryValue() {
-      return this.binaryValue.padStart(this.bits, "0");
-    },
-  },
-  methods: {
-    normalize(value) {
-      const maxValue = BigInt(2) ** BigInt(this.bits);
-      if (value >= BigInt(0)) {
-        return value % maxValue;
-      } else {
-        return (value + maxValue) % maxValue;
-      }
-    },
-    setBits(n) {
-      this.bits = n;
-    },
-    toggleBit(index) {
-      const bitValue = BigInt(2) ** BigInt(this.bits - index - 1);
-      this.value = this.normalize(this.value ^ bitValue);
-    },
-    increment() {
-      this.value = this.normalize(this.value + BigInt(1));
-    },
-    decrement() {
-      this.value = this.normalize(this.value - BigInt(1));
-    },
-    signedLeftShift() {
-      this.value = this.normalize(this.value * BigInt(2));
-    },
-    rightShift() {
-      this.value = this.normalize(this.value / BigInt(2));
-    },
-    signedRightShift() {
-      const binary = this.binaryValue.padStart(this.bits, "0");
-      let shifted = binary.slice(0, -1);
-      if (binary[0] === "1") {
-        shifted = "1" + shifted;
-      }
-      this.setBinaryValue(shifted);
-    },
-    not() {
-      const maxValue = BigInt(2) ** BigInt(this.bits) - BigInt(1);
-      this.value = maxValue - this.value;
-    },
-    swap() {
-      const binary = this.binaryValue.padStart(this.bits, "0");
-      const swapped = binary.match(/.{8}/g).reverse().join("");
-      this.setBinaryValue(swapped);
-    },
-    setUnsignedValue(val) {
-      try {
-        this.value = this.normalize(BigInt(val));
-      } catch {
-        // hack to reset everything to its normal value;
-        this.increment();
-        this.decrement();
-      }
-    },
-    setSignedValue(val) {
-      try {
-        this.value = this.normalize(BigInt(val));
-      } catch {
-        this.increment();
-        this.decrement();
-      }
-    },
-    setBinaryValue(val) {
-      try {
-        this.value = this.normalize(BigInt("0b" + val));
-      } catch {
-        this.increment();
-        this.decrement();
-      }
-    },
-    setHexValue(val) {
-      try {
-        this.value = this.normalize(BigInt("0x" + val));
-      } catch {
-        this.increment();
-        this.decrement();
-      }
-    },
-    onHashChange() {
-      const hash = window.location.hash.substring(3);
-      const hexRegex = /^[0-9A-Fa-f]+$/;
+const app = {
+    paddedBinaryValue: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    form() {
+        return tag.div({id: "app"},
+            tag.div({style: {display: "flex"}},
+                tag.div({style: {margin: "auto"}},
+                    tag.span({class: "toggle"}, " 8 bit "),
+                    tag.span({class: "toggle selected"}, " 16 bit "),
+                    tag.span({class: "toggle"}, " 32 bit "),
+                    tag.span({class: "toggle"}, " 64 bit ")
+                )
+            ),
 
-      if (hexRegex.test(hash)) {
-        const bits = hash.length * 4;
-        this.setBits(bits);
-        this.setHexValue(hash);
-      }
+            tag.div(
+                tag.div({class: "bit-pattern-wrapper"},
+                    /* I only implement 16 bits at this time (original: this is a mess but I couldn't think of a better way) */
+                    tag.div({class: "title", style: {"text-align": "center"}}, "Bit Pattern"),
+                    tag.div({class: "bit-container", style: {display: "flex"}},
+                        tag.div({style: {margin: "auto"}},
+                            tag.span({class: "byte"}, this.paddedBinaryValue.slice(0, 8).map( item => tag.span({ class: "bit"}, ""+item) ) ),
+                            tag.span({class: "byte"}, this.paddedBinaryValue.slice(8, 16).map( item => tag.span({ class: "bit"}, ""+item) ) )
+                        )
+                    )
+                ),
+
+                tag.div({class: "button-wrapper"},
+                    tag.button({class: "button", "data-tooltip": "Increment"}, "+"),
+                    tag.button({class: "button", "data-tooltip": "Decrement"}, "-"),
+                    tag.button({class: "button", "data-tooltip": "Left Shift"}, "<<"),
+                    tag.button({class: "button", "data-tooltip": "Signed Right Shift"}, ">>"),
+                    tag.button({class: "button", "data-tooltip": "Unsigned Right Shift"}, ">>>"),
+                    tag.button({class: "button", "data-tooltip": "NOT"}, "~"),
+                    tag.button({class: "button", "data-tooltip": "Swap byte order"}, "↔")
+                ),
+
+                tag.hr(),
+
+                tag.div({style: {display: "flex"}},
+                    tag.div({style: {margin: "auto"}, class: "values-wrapper"},
+                        tag.div(
+                            tag.div({class: "title"}, "Signed Value"),
+                            tag.div({class: "input-container"},
+                                tag.input({type: "text", class: "input-box", value: "0"})
+                            )
+                        ),
+
+                        tag.div(
+                            tag.div({class: "title"}, "Unsigned Value"),
+                            tag.div({class: "input-container"},
+                                tag.input({type: "text", class: "input-box", value: "0"})
+                            )
+                        ),
+
+                        tag.div(
+                            tag.div({class: "title"}, "Binary Value"),
+                            tag.div({class: "input-container"},
+                                tag.input({type: "text", class: "input-box", value: "0"})
+                            )
+                        ),
+
+                        tag.div(
+                            tag.div({class: "title"}, "Hexadecimal Value"),
+                            tag.div({class: "input-container"},
+                                tag.input({type: "text", class: "input-box", value: "0"})
+                            )
+                        )
+                    )
+                )
+            )
+        );
     },
-    setHash() {
-      window.location.hash = "0x" + this.hexValue.padStart(this.bits / 4, "0");
-    },
-  },
-  watch: {
-    value() {
-      this.setHash();
-    },
-    bits() {
-      this.value = this.normalize(this.value);
-      this.setHash();
-    },
-  },
-});
-app.mount("#app");
+    mount(container) {
+        document.querySelector(container).replaceWith(this.form())
+    }
+}
+app.mount('#app');
+
